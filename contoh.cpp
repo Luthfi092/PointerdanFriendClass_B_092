750ea5ed2c899b9a6468496a93f5a30c9c72403d
upi1682 <upi538287@gmail.com>
2025-05-06 15:32:40 +0700

    pointer1.1

--- contoh.cpp
+++ contoh.cpp
@@ -56,3 +56,33 @@ int main()
     objekPP.luasPp(objekPP.panjang, objekPP.lebar) << endl;
 
 }
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+class mahasiswa{
+    public:
+    int nim;
+    void showNim(){
+        cout<<"No Induk = "<<nim<<endl;
+    }
+};
+
+int main (){
+    mahasiswa mhs{1}; //Object mhs
+    mhs.showNim(); //Member Access Operator
+
+    mahasiswa &refMhs = mhs; //Pointer Reference refMhs
+    refMhs.nim = 2; //Member Access Operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; //pointer dereference pMhs
+    pMhs->nim = 3; // Arrow Operator
+    pMhs->showNim();
+    return 0;
+}
