5d5863c9b634bcd260eac09f7007ab4b035e1453
upi1682 <upi538287@gmail.com>
2025-05-06 15:50:52 +0700

    friendfunction1.1

--- friendclassSatu.cpp
+++ friendclassSatu.cpp
@@ -33,3 +33,41 @@ void siswa::setId(int pId)
 {
     id = pId;
 }
+
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+private:
+    string nama;
+
+public:
+    friend void setNama(mahasiswa &a, string);
+
+};
+
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+  
+}
+
+    int main()
+{
+    mahasiswa joko;
+    setNama(joko, "kairi Kumar");
+    return 0;
+}
